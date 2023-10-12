18a6894ac7db64267a746604af4ef70e77f54979
Javed <javedahmednizamani@gmail.com>
2023-10-13 00:38:33 +0500

    inserting data into db table

--- controller/students.create.js
+++ controller/students.create.js
@@ -3,8 +3,17 @@ var stds = db.std;
 
 const createStudents = async (req, res)=>{
     try{
-        // const {firstname, lastname, address, phone} = req.body;
+        const {firstname, lastname, address, phone} = req.body;
         console.log(req.body);
+
+        await stds.create({
+            firstname: firstname,
+            lastname: lastname,
+            address: address,
+            phone: phone 
+        }).then((result)=>{
+            res.status(200).send(result);
+        });
 
     }catch(error){
         console.error(error.stack);
