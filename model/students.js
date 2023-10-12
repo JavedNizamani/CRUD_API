8f38f46fa6c9bbf3ac073446b0e47837a94ad55e
Javed <javedahmednizamani@gmail.com>
2023-10-12 15:07:10 +0500

    setting_up_project

---
+++ model/students.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize)=>{
+
+    const Students = sequelize.define('students',{
+        firstname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        address: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    },{
+        sequelize,
+        tableName: 'students',
+        timestamps: false
+    });
+    return Students;
+}
---
+++ utils/db.tables.js
@@ -0,0 +1,10 @@
+const {Sequelize} = require('sequelize');
+const sequelize = require('./db');
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.std = require('../model/students')(sequelize, Sequelize);
+db.sequelize.sync();
+
+module.exports = db;
